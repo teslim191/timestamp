274fba8584b6dccea022697e5402f015a33137da
Teslim <teslimjimoh191@gmail.com>
2022-05-07 12:06:28 +0100

    route file created

--- index.js
+++ index.js
@@ -38,28 +38,7 @@ app.get("/api/hello", function (req, res) {
   // res.json({"unix": unix_date, "utc": date.toUTCString()})
 // })
 
-app.get('/api/:date?', (req, res) => {
-  const date_parse = Date.parse(req.params.date)
-  const regexp2 = /^(\d{0,})?$/;
-
-   let date = new Date(date_parse)
-  if (isNaN(date_parse) == false && req.params.date) {
-    res.json({"unix": date.getTime(), "utc": date.toUTCString()})
-  }
-  else if (!isNaN(date_parse)==true && !regexp2.test(req.params.date) && req.params.date) {
-    res.json({error:"Invalid Date" })
-  }
-  else if (regexp2.test(req.params.date) && req.params.date) {
-    date = new Date(Number(req.params.date));
-    res.json({"unix": date.getTime(), "utc": date.toString()})
-  }
-  else if (!date_parse){
-    current_date = new Date()
-    res.json({"unix": current_date.getTime(), "utc": current_date.toString()})
-   
-  }
-})
-
+app.use('/api', require('./routes/api'))
 
 // listen for requests :)
 var listener = app.listen(9000, function () {
---
+++ routes/api.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+
+
+
+router.get('/:date?', (req, res) => {
+    const date_parse = Date.parse(req.params.date)
+    const regexp2 = /^(\d{0,})?$/;
+  
+     let date = new Date(date_parse)
+    if (isNaN(date_parse) == false && req.params.date) {
+      res.json({"unix": date.getTime(), "utc": date.toUTCString()})
+    }
+    else if (!isNaN(date_parse)==true && !regexp2.test(req.params.date) && req.params.date) {
+      res.json({error:"Invalid Date" })
+    }
+    else if (regexp2.test(req.params.date) && req.params.date) {
+      date = new Date(Number(req.params.date));
+      res.json({"unix": date.getTime(), "utc": date.toString()})
+    }
+    else if (!date_parse){
+      current_date = new Date()
+      res.json({"unix": current_date.getTime(), "utc": current_date.toString()})
+     
+    }
+  })
+
+module.exports = router
